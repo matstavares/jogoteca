be68cd44b01374a194bdfd2bea379b45e5e88847
Mateus Tavares <maseta.mateus@gmail.com>
2025-06-18 19:14:13 -0300

    separado e com CRUD

--- jogoteca.py
+++ jogoteca.py
@@ -1,90 +1,15 @@
-from flask import Flask, render_template, request, redirect, session, flash, url_for
+# autopep8: off
 
-
-class Jogo:
-    def __init__(self, nome, categoria, console):
-        self.nome = nome
-        self.categoria = categoria
-        self.console = console
-
-
-jogo1 = Jogo("God of War", "Ação", "PS2")
-jogo2 = Jogo("Elden Ring", "RPG", "PC")
-jogo3 = Jogo("Avowed", "RPG", "PC")
-
-jogos = [jogo1, jogo2, jogo3]
-
-
-class Usuario:
-    def __init__(self, nome, nickname, senha):
-        self.nome = nome
-        self.nickname = nickname
-        self.senha = senha
-
-
-usuario1 = Usuario("Bruno Divino", "BD", "alohomora")
-usuario2 = Usuario("Camila Ferreira", "Mila", "ola")
-usuario3 = Usuario("Guilherme Louro", "Cake", "oi")
-
-usuarios = {
-    usuario1.nickname: usuario1,
-    usuario2.nickname: usuario2,
-    usuario3.nickname: usuario3,
-}
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.secret_key = "aquilo"
+app.config.from_pyfile('config.py')
 
+db = SQLAlchemy(app)
 
-@app.route("/")
-def index():
-    return render_template("lista.html", titulo="Jogos", jogos=jogos)
+from views import *
 
-
-@app.route("/novo")
-def novo():
-    if "usuario_logado" not in session or session["usuario_logado"] == None:
-        return redirect(url_for("login", proxima=url_for("novo")))
-    return render_template("novo.html", titulo="Novo Jogo")
-
-
-@app.route("/criar", methods=["POST"])
-def criar():
-    nome = request.form["nome"]
-    categoria = request.form["categoria"]
-    console = request.form["console"]
-
-    jogo = Jogo(nome, categoria, console)
-    jogos.append(jogo)
-
-    return redirect(url_for("index"))
-
-
-@app.route("/login")
-def login():
-    proxima = request.args.get("proxima")
-    return render_template("login.html", proxima=proxima)
-
-
-@app.route("/autenticar", methods=["POST"])
-def autenticar():
-    if request.form["usuario"] in usuarios:
-        usuario = usuarios[request.form["usuario"]]
-        if request.form["senha"] == usuario.senha:
-            session["usuario_logado"] = usuario.nickname
-            flash(usuario.nickname + " logado com sucesso!")
-            proxima_pagina = request.form["proxima"]
-            return redirect(proxima_pagina)
-    flash("Usuario não logado!")
-    return redirect(url_for("login"))
-
-
-@app.route("/logout")
-def logout():
-    session["usuario_logado"] = None
-    flash("Usuario deslogado com sucesso!")
-    return redirect(url_for("index"))
-
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
