bdd70dbaad1a60b7c775fa6e5b9c140eb6b67f2f
Mateus Tavares <maseta.mateus@gmail.com>
2025-07-30 15:33:34 -0300

    funcionando com WTF

--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 import os
 from jogoteca import app
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators
+from wtforms import StringField, SubmitField, PasswordField, validators
 
 
 class FormularioJogo(FlaskForm):
@@ -13,6 +13,23 @@ class FormularioJogo(FlaskForm):
         "Categoria", [
             validators.DataRequired(), validators.Length(
                 min=1, max=40)])
+    console = StringField(
+        "Console", [
+            validators.DataRequired(), validators.Length(
+                min=1, max=20)])
+    salvar = SubmitField("Salvar")
+
+
+class FormularioUsuario(FlaskForm):
+    apelido = StringField(
+        "Apelido", [
+            validators.DataRequired(), validators.Length(
+                min=1, max=8)])
+    senha = PasswordField(
+        "Senha", [
+            validators.DataRequired(), validators.Length(
+                min=1, max=100)])
+    login = SubmitField("Login")
 
 
 def recupera_imagem(id):
@@ -24,6 +41,8 @@ def recupera_imagem(id):
 
 
 def deleta_arquivo(id):
-    arquivo = recupera_imagem(id)
-    if arquivo != 'capa_padrao.jpg':
-        os.remove(os.path.join(app.config['UPLOAD_PATH'], arquivo))
+    nome_arquivo = recupera_imagem(id)
+    if nome_arquivo != 'capa_padrao.jpg':
+        os.remove(os.path.join(app.config['UPLOAD_PATH'], nome_arquivo))
+    print("teste")
+    print(os.path.join(app.config['UPLOAD_PATH'], nome_arquivo))
